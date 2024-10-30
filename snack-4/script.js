d990ae65b2fe398620acaab81096687ce47bc553
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-30 16:47:36 +0100

    add

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,5 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const authorsName = posts.map(({ author }) => author);
+console.log(authorsName);
